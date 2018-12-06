41041d6eb78b34b47032e41730fd778bc4f11671
guacke <Guillaume.Acke@UGent.be>
2018-12-06 16:48:48 +0100

    Swap victini for golett.

--- vscentrum/install.sh
+++ vscentrum/install.sh
@@ -4,12 +4,12 @@
 # Due to cluster specific optimizations, each supported cluster gets its own directory.
 # The compiled binaries can be found under ${VSC_DATA}/apps/${VSC_INSTITUTE_CLUSTER}/gqcg/local
 
-# Compile on delcatty
+echo "Init compilation on victini."
+module swap cluster/victini 2> /dev/null
+qsub per_cluster_install.sh
+
 echo "Init compilation on delcatty."
 module swap cluster/delcatty 2> /dev/null
 qsub per_cluster_install.sh
 
-# Compile on golett
-echo "Init compilation on golett."
-module swap cluster/golett 2> /dev/null
-qsub per_cluster_install.sh
+
