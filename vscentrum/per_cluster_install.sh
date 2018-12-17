fb12fb18a6820cd2f075fca3e54a0f7910ab3f0b
guacke <Guillaume.Acke@UGent.be>
2018-12-17 12:27:51 +0100

    Reduce installation to gqcp.

--- vscentrum/per_cluster_install.sh
+++ vscentrum/per_cluster_install.sh
@@ -35,22 +35,22 @@ INSTALL_PREFIX=${VSC_DATA}/apps/${VSC_INSTITUTE_CLUSTER}/gqcg/local
 
 rm -rf ${SOURCE_PREFIX} && mkdir -p ${SOURCE_PREFIX} && cd ${SOURCE_PREFIX}
 
-# 0. Google benchmark
+# # 0. Google benchmark
 #rm -rf benchmark
 #git clone https://github.com/tmhuysen/benchmark.git
 #(cd benchmark && git clone https://github.com/google/googletest.git && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} && make -j ${PPN} && make install)
 
-# 1. cpputil
-rm -rf cpputil
-git clone https://github.com/GQCG/cpputil.git --branch develop
-(cd cpputil && rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} && make -j ${PPN} && make test ARGS=-j${PPN} && make install)
+# # 1. cpputil
+# rm -rf cpputil
+# git clone https://github.com/GQCG/cpputil.git --branch develop
+# (cd cpputil && rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} && make -j ${PPN} && make test ARGS=-j${PPN} && make install)
 
-# 2. numopt
-rm -rf numopt
-git clone https://github.com/GQCG/numopt.git --branch develop --recurse-submodules
-(cd numopt && rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DUSE_MKL=ON && make -j ${PPN} && make test ARGS=-j${PPN} && make install)
+# # 2. numopt
+# rm -rf numopt
+# git clone https://github.com/GQCG/numopt.git --branch develop --recurse-submodules
+# (cd numopt && rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DUSE_MKL=ON && make -j ${PPN} && make test ARGS=-j${PPN} && make install)
 
 # 3. gqcp
 rm -rf gqcp
-git clone https://github.com/GQCG/gqcp.git --branch develop
+git clone https://github.com/GQCG/gqcp.git --branch develop --recurse-submodules
 (cd gqcp && rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DLIBINTROOT=${LIBINT_ROOT} -DUSE_MKL=ON && make -j ${PPN} && make test ARGS=-j${PPN} && make install)
